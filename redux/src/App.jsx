7add7ac4f776bdb55160c7830a1ad64999b7b0bc
vidhvath28 <vidhvathj.2005@gmail.com>
2023-12-22 12:16:18 +0530

    update

---
+++ redux/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Actions from './components/Actions'
+import LikeCounter from './components/LikeCounter';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Actions />
+        <LikeCounter/>
+      </div>
+    </>
+  );
+}
+
+export default App
